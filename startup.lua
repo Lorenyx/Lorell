c73d369714b5cce453923faf52793edbb03f8117
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-12-26 21:42:30 -0600

    Fixed if exists

--- startup.lua
+++ startup.lua
@@ -1,12 +1,12 @@
 -- default program for "myLorell" devices
 local _key = "<KEY_HERE>"
 
-if not exists("cloud.lua") then
+if not fs.exists("cloud.lua") then
     print("Downloading cloud.lua")
     shell.run("wget", "https://cloud-catcher.squiddev.cc/cloud.lua")
 end -- if not exists
 
-if not exists("client.lua") then
+if not fs.exists("client.lua") then
     print("Downloading client.lua")
     shell.run("wget", "https://raw.githubusercontent.com/Lorenyx/Lorell/main/client.lua")
 end -- if not exists
