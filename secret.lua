3bd72383fb7011ae634570288ca962600a0e25bd
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-12-28 13:40:55 -0600

    Add authenticate()

--- secret.lua
+++ secret.lua
@@ -1,6 +1,5 @@
 -- secret.lua
 -------------
-
 local secret = {}
 
 --IMPORTANT: Remove any test users before pushing
@@ -11,5 +10,14 @@ local users = {
 function secret.authorize(name, token)
     return users[name] == token
 end -- function secret.authorize()
+
+function secret.authenticate(name, token)
+    if not users[name] then
+        users[name] = token
+        return true
+    else
+        return false
+    end -- if not users[name]
+end -- function secret.authenticate
 
 return secret
