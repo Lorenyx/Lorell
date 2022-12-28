7af4eb90521f96d218a6275ebe93c6fa4ce2467b
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-12-27 23:55:51 -0600

    Added token creation

--- startup.lua
+++ startup.lua
@@ -1,15 +1,62 @@
 -- default program for "myLorell" devices
-local _key = "<KEY_HERE>"
+--> URL to download client script
+local _URL = "https://raw.githubusercontent.com/Lorenyx/Lorell/main/client.lua"
+--> name of client script
+local _SCRIPT = "lorell.lua"
+--> name of update notifaction
+local _UPDATE = ".UPDATE_LORELL"
+--> name of token file
+local _TOKEN = ".TOKEN_LORELL"
 
-if not fs.exists("cloud.lua") then
-    print("Downloading cloud.lua")
-    shell.run("wget", "https://cloud-catcher.squiddev.cc/cloud.lua")
-end -- if not exists
 
-if not fs.exists("client.lua") then
-    print("Downloading client.lua")
-    shell.run("wget", "https://raw.githubusercontent.com/Lorenyx/Lorell/main/client.lua")
-end -- if not exists
+function init()
+    init_token()
+    init_client()
+    shell.run(_SCRIPT)
+end -- function init
 
-shell.run("cloud.lua", _key)
-shell.run("client.lua")
+
+function init_client()
+    -- nested function
+    function get_script()
+        shell.run("wget", _URL, _SCRIPT)
+    end -- function get_script
+    -- Check for client
+    if not fs.exists(_SCRIPT) then
+        print("Downloading client...")
+        get_script()
+    elseif fs.exists(_UPDATE) then
+        print("Updating client...")
+        fs.delete(_SCRIPT)
+        fs.delete(_UPDATE)
+        get_script()
+    end -- if not exists
+end -- function init_client
+
+
+function init_token()
+    function get_token()
+        -- generate uuid
+        -- DO NOT SHARE SEED
+        local _seed = os.epoch()
+        -- local _seed = os.time() -- FOR TESTING ONLY
+        math.randomseed(_seed)
+        --> Available letters for encoding
+        local A = "abcdefghijklmnopqrstuvwxyz"
+        A = A..A:upper().."1234567890"
+        assert(#A == (26+26+10), "Alphabet is incorrect length!")
+        return string.gsub(string.rep('x', 32), 'x', function()
+            local k = math.random(#A)
+            return string.format('%s', A:sub(k,k+1))
+        end) -- function()
+    end -- function get_token
+    -- Check for token
+    if not fs.exists(_TOKEN) then
+        local token, length = get_token()
+        assert(length == 32, "Key is incorrect length!")
+        local f = fs.open(_TOKEN, "w")
+        f.write(token)
+    end -- if not fs.exists
+end -- function init_token()
+-- Main Execution
+init()
