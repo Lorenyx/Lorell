941bba50f787dbee3e28a118f00544be0a701c66
Lore <30599612+Lorenyx@users.noreply.github.com>
2023-01-02 21:05:40 -0600

    update startup

--- startup.lua
+++ startup.lua
@@ -11,6 +11,20 @@ if fs.exists(config.lorell.update) then
     shell.run("wget", "https://raw.githubusercontent.com/Lorenyx/Lorell/main/client.lua")
 end -- if fs.exists(config.lorell.update)
 
+if ccemux then 
+    local random = math.random
+    ccemux.attach("back", "wireless_modem", {
+        -- The range of this modem
+        range = 64,
+        -- Whether this is an ender modem
+        interdimensional = false,
+        -- The current world's name. Sending messages between worlds requires an interdimensional modem
+        world = "main",
+        -- The position of this wireless modem within the world
+        posX = random(32), posY = random(32), posZ = random(32),
+    })
+end -- if ccemux
+
 -- Clear screen and start
 term.clear()
 shell.run(config.lorell.purpose)
